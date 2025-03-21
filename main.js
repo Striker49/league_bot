8746ab5bf6107b6bdf8732fdb51fb8413b37881e
Sebastien <roy_sebastien@hotmail.fr>
2025-03-21 19:01:59 -0400

    added active game info, champion id mapped with name w ddragon

--- main.js
+++ main.js
@@ -5,13 +5,20 @@ const RIOT_API_KEY = process.env.RIOT_API_KEY;
 
 let puuid;
 let games = {};
-let winLoss = {};
+let gameData = {};
+let playerId;
+let championId;
+let championMap;
+let activeGame = {};
 
 async function getSummoner(summonerName, tag) {
-    const url = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tag}?api_key=${RIOT_API_KEY}`;
+    summonerName = summonerName.replace(" ", "%20");
+    const url = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tag}`;
+    const headers = { "X-Riot-Token":RIOT_API_KEY };
+
 
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(url, { headers });
         console.log(response.data);
         puuid = response.data["puuid"];
     } catch (error) {
@@ -22,13 +29,17 @@ async function getSummoner(summonerName, tag) {
 }
 
 async function getGames() {
-    url = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?&api_key=${RIOT_API_KEY}`;
-    console.log("2nd f", puuid);
+    url = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids`;
+    const headers = { 
+        headers: { 
+            "X-Riot-Token": RIOT_API_KEY 
+        }
+    };
+    //console.log("Last 20 games", puuid);
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(url, headers);
         console.log(response.data);
         games = response.data;
-        console.log("games: ", games);
 
     } catch (error) {
         console.error("Error fetching summoner:", error.response.data);
@@ -39,17 +50,86 @@ async function getGames() {
 }
 
 async function getWinOrLoss() {
-    console.log("games2: ", games[1]);
-    url = `https://americas.api.riotgames.com/lol/match/v5/matches/${games[0]}?&api_key=${RIOT_API_KEY}`;
+    console.log("Last Game info: ", games[0]);
+    url = `https://americas.api.riotgames.com/lol/match/v5/matches/${games[0]}`;
+    const headers = { 
+        headers: { 
+            "X-Riot-Token":RIOT_API_KEY 
+        }
+    };
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(url, headers);
         console.log(response.data);
-        winLoss = response.data;
+        gameData = response.data;
     } catch (error) {
         console.error("Error fetching summoner:", error.response.data);
         console.error("RIOT_API_KEY", RIOT_API_KEY);
     }
-    console.log("final", winLoss.info.teams[1].win);
+    let i;
+    for (i = 0; gameData.metadata.participants[i]; i++) {
+        if (gameData.metadata.participants[i] == puuid) {
+            playerId = i;
+            break;
+        }
+    }
+    console.log("Last Game details:");
+    console.log("gameMode", gameData.info.gameMode);
+    console.log("Summoner", gameData.info.participants[i].riotIdGameName);
+    console.log("Champion", gameData.info.participants[i].championName);
+    console.log("Role", gameData.info.participants[i].role);
+    console.log("win", gameData.info.participants[i].win);
+    getActiveGame()
 }
 
-getSummoner("Amado%20III", "Jimmy");
+async function getActiveGame() {
+    console.log("\nChecking Active Game: ");
+    url = `https://na1.api.riotgames.com/lol/spectator/v5/active-games/by-summoner/${puuid}`;
+    const headers = { 
+        headers: { 
+            "X-Riot-Token":RIOT_API_KEY 
+        }
+    };
+    try {
+        const response = await axios.get(url, headers);
+        activeGame = response.data;
+    } catch (error) {
+        console.error("Error fetching summoner:", error.response.data);
+    }
+    let i;
+    if (!activeGame.gameId)
+        console.log("No game found..!");
+    else {
+        for (i = 0; activeGame.participants[i]; i++) {
+        if (activeGame.participants[i].puuid == puuid) {
+            playerId = i;
+            championId = activeGame.participants[i].championId;
+            break;
+            }
+        }
+        await fetchChampionId();
+        console.log("In Game");
+        console.log(activeGame.gameMode);
+        console.log(activeGame.gameType);
+        console.log(championMap[championId]);
+    }
+
+}
+
+async function fetchChampionId() {
+    url = "https://ddragon.leagueoflegends.com/cdn/14.5.1/data/en_US/champion.json";
+
+    try {
+        const response = await axios.get(url);
+        const ddragonData = response.data.data;
+        championMap = Object.values(ddragonData).reduce((map, champ) => {
+            map[parseInt(champ.key)] = champ.name;
+            return map;
+        }, {});
+    }
+    catch (error) {
+        console.error("Error fetching champion Ids");
+    }
+}
+
+//fetchChampionId();
+getSummoner("astrasepram", "NA1");
